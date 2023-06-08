4e221ba9c45596973e0d2c34f2b0a8d5e92baec7
JennyLiu0716 <106459935+JennyLiu0716@users.noreply.github.com>
2023-06-08 21:13:52 +0800

    Update UnitIntervalGenerator.java

--- project/src/graph/generation/UnitIntervalGenerator.java
+++ project/src/graph/generation/UnitIntervalGenerator.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
 * To generate a unit interval graph, it suffices to specify the last neighbor of each vertex, such that the sequence is not non-decreasing.
@@ -25,19 +26,90 @@ public class UnitIntervalGenerator {
         int lastNeighbors[] = new int[n];
         
         Random rand = new Random();
-        int cur = 0;
+        int cur = 1;
         for (int i = 0; i < n && cur < n - 1; i++) {
-            cur = cur + rand.nextInt(n - cur);
+        	// make sure this is a connected graph
+        	if (cur<i+1) cur=i+1;
+        	cur = cur + rand.nextInt(n - cur);
             lastNeighbors[i] = cur;
         }
+        for (int i=n-1;i>=0;i--) {
+        	if (lastNeighbors[i]==0) {
+        		lastNeighbors[i]=n-1;
+        	}else {
+        		break;
+        	}
+        }
+//      System.out.println(Arrays.toString(lastNeighbors));
+
         return lastNeighbors;
   }
+    
+    /**
+     * generate a random order @numbers for vertices
+     * this permutation is used to shuffle the numbers of the vertices 
+     * @param n: number of vertices
+     * @return permutation
+     */
+    static int[] randomOrder(int n) {
+		int nodeNum = n;
+		int numbers[] = new int[nodeNum];
+		
+		// ordered list
+		for(int i=0;i<nodeNum;i++) {
+			numbers[i] = i+1;
+		}
+		// Iterate through the array elements in a for loop
+		// Use the Random class to generate a random index number. 
+		// Then swap the current index element with the randomly generated index element. 
+		// At the end of the for loop, we will have a randomly shuffled array.
+		// ref: https://www.digitalocean.com/community/tutorials/shuffle-array-java
+		Random rand = new Random();
+		
+		for (int i = 0; i < nodeNum; i++) {
+			int randomIndexToSwap = rand.nextInt(nodeNum);
+			int temp = numbers[randomIndexToSwap];
+			numbers[randomIndexToSwap] = numbers[i];
+			numbers[i] = temp;
+		}
+		return numbers;
+		
+//		System.out.println(Arrays.toString(numbers));
+    }
 
-   /**
+    /**
      * Write the generated graph into a file
      * Shuffle the numbers of the vertices.
      */
-    static void write(int[] lastNeighbors, String filename) {
+    static void write(int[] lastNeighbors, String filename,int[]numbers) {
+    	int nodeNum = lastNeighbors.length;
+		// calculate edgeNum
+		int edgeNum = 0;
+		for(int i=0;i<nodeNum;i++) {
+			edgeNum+=lastNeighbors[i]-i;
+		}
+		
+		// given the permutation @numbers and the position @lastNeighbors
+		// write the information to text file
+		
+    	try {
+	        FileWriter myWriter = new FileWriter(filename);
+	        myWriter.write("c this is the interval graph \n");
+	        myWriter.write("p edge "+nodeNum+" "+edgeNum+"\n");
+	        for (int i=0;i<nodeNum;i++) {
+	        	for(int j=i+1;j<=lastNeighbors[i];j++) {
+	        		myWriter.write("e "+numbers[i]+" "+numbers[j]+"\n");
+	        		edgeNum++;
+	        	}
+	        }	
+	        myWriter.close();
+	        System.out.println("successful generation");
+//	        
+	   }
+	    catch (IOException e) {
+	        System.out.println("An error occurred.");
+	        e.printStackTrace();
+	    }
     }
 
     /**
@@ -51,14 +123,35 @@ public class UnitIntervalGenerator {
      * @param number: the number of graphs to be generated
      */
     public static void generate(String path, String prefix, int n, boolean random, int number) {
+    	// filename format
+    	int length = (int) (Math.log10(number) + 1);
+    	String format = "%0"+length+"d";
+    	// generate "number" graphs
         for (int i = 1; i <= number; i++) {
             int n0 = n;
+            // if random, there are at most n vertices
             if (random) {
                 Random rand = new Random();
-                n0 = rand.nextInt(n);
+                n0 = rand.nextInt(n)+1;
             }
+            //generate last neighbors and write into files
+            String filename = path+prefix+String.format(format, i) +".txt";
+            int[] lastNeighbors = generate(n0);
+        	int nodeNum = lastNeighbors.length;
+        	// get the new order for the nodes
+        	// seperate 2 functions for JUnit test
+        	int numbers[] = randomOrder(nodeNum);
+            write(lastNeighbors,filename,numbers);
         }
     }
+    
+//    public static void main(String[] args) {
+////    	generate(10);
+////    	int[] temp = new int[10];
+////    	write(temp,"asd");
+//    	String path = "./graphs/";
+//    	generate(path, "uig",10,true,10);
+//    }
 
         
 }
