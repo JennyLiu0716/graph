fd3af8560180733222d604d164b3b2d81e5c351b
yixincao <44076104+yixincao@users.noreply.github.com>
2023-06-20 20:38:05 +0800

    Update GraphSearch.java

--- project/src/graph/algorithms/GraphSearch.java
+++ project/src/graph/algorithms/GraphSearch.java
@@ -19,7 +19,7 @@ import utils.DList;
 public class GraphSearch {
     /**
      * To find an end vertex of unit interval graph using BFS:
-     * a vertex in the last level with the minimum degree is an end vertex (Lemma 3.7 of Cao 2021).
+     * a vertex in the last level with the minimum degree is an end vertex (Lemma 3.7 of [Cao 2021]).
      * 
      * @param g: the input graph
      * @return endVertex: an end vertex
